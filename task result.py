a357e4902e0501516378510790ae9dd1282e7091
Kirill Gaposhkin <130587425+Kirill5github@users.noreply.github.com>
2023-05-15 12:45:47 +0300

    Create task result.py

---
+++ task result.py
@@ -0,0 +1,13 @@
+price = 0
+tickets = int(input("Введите количество билетов:"))
+for age in range(tickets):
+    age = int(input("Введите возраст"))
+    if age < 18:
+        price += 0
+    elif 18 <= age < 25:
+        price += 990
+    elif age >= 25:
+        price += 1390
+if tickets > 3:
+    price -= price/100*10
+print("Полная стоимость билетов:", price)
